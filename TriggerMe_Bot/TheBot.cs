c582f6dab8e949cee730b8c8a692844962b55909
Rumir <rumir@micronited.de>
2018-10-25 23:04:18 +0200

    Added Help Text and "Not Implemented yet" message

--- TriggerMe_Bot/TheBot.cs
+++ TriggerMe_Bot/TheBot.cs
@@ -67,12 +67,37 @@ namespace TriggerMe_Bot
                         ", you can just write \"Trigger Help!\" to trigger me and write a helpful help message in this channel");
                     break;
                 case "trigger help!":
-                    await message.Channel.SendMessageAsync("**Commands**\n" +
+                    await message.Channel.SendMessageAsync("***Commands***\n" +
                         "• Trigger Me! - Shows a helpful introduction to me\n" +
                         "• Trigger Help! - Shows this helpful help message\n" +
-                        "• Trigger List! - Messages you privately the full trigger list. You need to allow me to message you or I can't help you with this\n" +
-                        "• Trigger List2! - Shows the full trigger list in this channel\n" +
-                        "• Trigger Permissions! - Shows ");
+                        "• Trigger List! - Messages you privately the full trigger list. You need to allow me to message you or I can't help you with this\n" + //TODO
+                        "• Trigger List2! - Shows the full trigger list in this channel\n" + //TODO
+                        "• Trigger Permissions! - Shows all the needed permissions in a pretty list\n\n" + //TODO
+                        "***Admin Commands (need to be at least moderator)***\n" +
+                        "• Trigger Add <Word or Words> <Mode>! - Adds one word or a sequence of words to the Trigger List. The Mode parameter accepts \"normal\"" + //TODO
+                        ", \"characters\" where...\n" +
+                        "•• normal - no special characters for letters\n" + //TODO
+                        "•• characters - included special characters for letters like \"Tr!gg3r\"\n" + //TODO
+                        "• Trigger Info <Word or Words>! - Shows infos about the set trigger\n" + //TODO
+                        "• Trigger Remove <Word or Words>! - Removes a trigger\n" + //TODO
+                        "• Trigger AddMod <Discord Tag>! - Adds a moderator **(OWNER ONLY)**\n" + //TODO
+                        "• Trigger ListMod! - Lists all moderators **(OWNER ONLY)**\n" + //TODO
+                        "• Trigger RemoveMod <Discord Tag>! - Removes a moderator **(OWNER ONLY)**"); //TODO
+                    break;
+                case "trigger list!":
+                case "trigger list2!":
+                case "trigger permissions!":
+                    await message.Channel.SendMessageAsync("Not implemented yet");
+                    break;
+            }
+            if(message.Content.ToLower().Contains("trigger add")
+                || message.Content.ToLower().Contains("trigger info")
+                || message.Content.ToLower().Contains("trigger remove")
+                || message.Content.ToLower().Contains("trigger addmod")
+                || message.Content.ToLower().Contains("trigger listmod")
+                || message.Content.ToLower().Contains("trigger removemod"))
+            {
+                await message.Channel.SendMessageAsync("Not implemented yet");
             }
             
         }
