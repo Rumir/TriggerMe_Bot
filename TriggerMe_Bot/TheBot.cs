b068110a4e4846d07873bde01b49cceb7c08353f
Rumir <s.schamey@gmail.com>
2018-10-27 14:18:44 +0200

    Added Permissions needed

--- TriggerMe_Bot/TheBot.cs
+++ TriggerMe_Bot/TheBot.cs
@@ -50,11 +50,19 @@ namespace TriggerMe_Bot
 
         private async Task MessageReceivedAsync(SocketMessage message)
         {
-            // The bot should never respond to itself.
-            if (message.Author.Id == _client.CurrentUser.Id)
-                return;
+            try
+            {
+                // The bot should never respond to itself.
+                if (message.Author.Id == _client.CurrentUser.Id)
+                    return;
 
-            await CommandTrigger(message);
+                await CommandTrigger(message);
+            }
+            catch
+            {
+                await message.Channel.SendMessageAsync("Something went wrong horribly. Please contact a developer: https://github.com/Rumir/TriggerMe_Bot/issues");
+            }
+            
         }
 
         private async Task CommandTrigger(SocketMessage message)
@@ -73,8 +81,7 @@ namespace TriggerMe_Bot
                         "• Trigger Help! - Shows this helpful help message\n" +
                         "• Trigger List! - Messages you privately the full trigger list. You need to allow me to message you or I can't help you with this\n" + 
                         "• Trigger List2! - Shows the full trigger list in this channel\n" + 
-                        "• Trigger Permissions! - Shows all the needed permissions in a pretty list\n\n" + //TODO
-                        "***Admin Commands (need to be at least moderator)***\n" +
+                        "***Admin Commands (need to be at least moderator)***\n\n" +
                         "• Trigger Add <Word or Words> <Mode>! - Adds one word or a sequence of words to the Trigger List. The Mode parameter accepts \"normal\"" + //TODO
                         ", \"characters\" where...\n" +
                         "•• normal - no special characters for letters\n" + //TODO
@@ -83,7 +90,8 @@ namespace TriggerMe_Bot
                         "• Trigger Remove <Word or Words>! - Removes a trigger\n" + //TODO
                         "• Trigger AddMod <Discord Tag>! - Adds a moderator **(OWNER ONLY)**\n" + //TODO
                         "• Trigger ListMod! - Lists all moderators **(OWNER ONLY)**\n" + //TODO
-                        "• Trigger RemoveMod <id>! - Removes a moderator **(OWNER ONLY)**\n\n" + //TODO
+                        "• Trigger RemoveMod <id>! - Removes a moderator **(OWNER ONLY)**\n" + //TODO
+                        "• Trigger Permissions! - Shows all the needed permissions in a pretty list\n\n" +
                         "***Notice***\n" +
                         "• All commands are case-insensitive. So you can write everything capitalized, or the opposite, or just a mix of both.\n" +
                         "• I am still under construction. Please be patient with me when I crash. My programmer will fix the problems as fast as possible."); 
@@ -109,7 +117,7 @@ namespace TriggerMe_Bot
                         if (output.Length >= 1984)
                             await message.Channel.SendMessageAsync("Output string too long. Please contact a developer");
                         else
-                            await message.Channel.SendMessageAsync("***Triggers***\n\n" + output);
+                            await message.Channel.SendMessageAsync("***Triggers***\n" + output);
                     }
                     break;
 
@@ -133,20 +141,30 @@ namespace TriggerMe_Bot
                         if (output.Length >= 1984)
                             await message.Author.SendMessageAsync("Output string too long. Please contact a developer");
                         else
-                            await message.Author.SendMessageAsync("***Triggers***\n\n" + output);
+                            await message.Author.SendMessageAsync("***Triggers***\n" + output);
                     }
                     break;
 
                 case "trigger permissions!":
-                    await message.Channel.SendMessageAsync("Not implemented yet");
+                    await message.Channel.SendMessageAsync("***Permissions Needed***\n" +
+                        "• View Channels\n" +
+                        "• Send Messages\n" +
+                        "• Embed Links\n" +
+                        "• Attach Files\n" +
+                        "• Manage Messages\n" +
+                        "• Read Message History\n" +
+                        "• Use External Emojis\n" +
+                        "• Add Reactions\n");
                     break;
             }
-            if(message.Content.ToLower().Contains("trigger add")
-                || message.Content.ToLower().Contains("trigger info")
-                || message.Content.ToLower().Contains("trigger remove")
-                || message.Content.ToLower().Contains("trigger addmod")
-                || message.Content.ToLower().Contains("trigger listmod")
-                || message.Content.ToLower().Contains("trigger removemod"))
+            if (1 == 0)
+                ;
+            else if (message.Content.ToLower().Contains("trigger add")
+                 || message.Content.ToLower().Contains("trigger info")
+                 || message.Content.ToLower().Contains("trigger remove")
+                 || message.Content.ToLower().Contains("trigger addmod")
+                 || message.Content.ToLower().Contains("trigger listmod")
+                 || message.Content.ToLower().Contains("trigger removemod"))
             {
                 await message.Channel.SendMessageAsync("Not implemented yet");
             }
